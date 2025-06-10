8dc626e6bacd93595b8e9d7dbc69cfa3f68133d7
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2025-06-10 22:27:38 +0700

    Add Album

---
+++ Controllers/AlbumController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Music.Data;
+
+namespace Music.Controllers
+{
+    public class AlbumController(MusicDbContext context) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var albums = await context.Albums.AsNoTracking().ToListAsync();
+            return View(albums);
+        }
+    }
+}
